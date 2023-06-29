115f25e91ef1129a23d562a4512a7e846d16e11b
Degreat <mail@degreat.co.uk>
2023-06-29 12:23:03 +0000

    Button config form

--- app/src/sections/Buttons.tsx
+++ app/src/sections/Buttons.tsx
@@ -1,9 +1,15 @@
+import Card from '../components/Card'
+import Fieldset from '../components/Fieldset'
 import Section from '../components/Section'
+import TextInput from '../components/TextInput'
+import UnitInput from '../components/UnitInput'
+import cs from './Common.module.css'
+import styles from './Buttons.module.css'
 
 function Buttons() {
   return (
     <Section
-      config={null}
+      config={<Config />}
       id="button"
       name={
         <>
@@ -17,15 +23,39 @@ function Buttons() {
   )
 }
 
+function Config() {
+  return (
+    <Card>
+      <header className={cs.configHeader}>buttons</header>
+
+      <Fieldset label="Font family">
+        <UnitInput />
+      </Fieldset>
+
+      <Fieldset label="Font size">
+        <UnitInput />
+      </Fieldset>
+
+      <Fieldset label="Padding">
+        <TextInput />
+      </Fieldset>
+
+      <Fieldset label="Border radius">
+        <UnitInput />
+      </Fieldset>
+    </Card>
+  )
+}
+
 function Preview() {
   return (
-    <>
+    <div class={styles.preview}>
       <div>
         <button>Base/Default</button>
         <button className="primary">Primary</button>
         <button className="accent">Accent</button>
       </div>
-    </>
+    </div>
   )
 }
 
