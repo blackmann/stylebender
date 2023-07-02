7066a25a76a34b4c3fa3bceb38e899860f48f8e5
William M. Tsikata <tsikatawill@gmail.com>
2023-07-02 18:18:27 +0000

    set input styles on change

--- app/src/sections/Input.tsx
+++ app/src/sections/Input.tsx
@@ -1,3 +1,4 @@
+import { getStyle, setStyle } from '../config'
 import Card from '../components/Card'
 import ColorPicker from '../components/ColorPicker'
 import ComponentHeader from '../components/ComponentHeader'
@@ -33,35 +34,100 @@ function Config() {
       </header>
 
       <Fieldset label="Font Family" inputId="input-font-family">
-        <TextInput id="input-font-family" />
+        <TextInput
+          id="input-font-family"
+          onChange={(e) =>
+            setStyle(
+              'input.fontFamily',
+              (e.target as HTMLInputElement).value,
+              true
+            )
+          }
+          value={getStyle('input.fontFamily')}
+        />
       </Fieldset>
 
       <Fieldset label="Font Size" inputId="input-font-size">
-        <UnitInput id="input-font-size" />
+        <UnitInput
+          id="input-font-size"
+          onChange={(e) =>
+            setStyle(
+              'input.fontSize',
+              (e.target as HTMLInputElement).value,
+              true
+            )
+          }
+          value={getStyle('input.fontSize')}
+        />
       </Fieldset>
 
       <Fieldset label="Font Weight" inputId="input-font-weight">
-        <UnitInput id="input-font-weight" />
+        <UnitInput
+          id="input-font-weight"
+          onChange={(e) =>
+            setStyle(
+              'input.fontWeight',
+              (e.target as HTMLInputElement).value,
+              true
+            )
+          }
+          value={getStyle('input.fontWeight')}
+        />
       </Fieldset>
 
       <Fieldset label="Padding" inputId="input-padding">
-        <TextInput id="input-padding" />
+        <TextInput
+          id="input-padding"
+          onChange={(e) =>
+            setStyle(
+              'input.padding',
+              (e.target as HTMLInputElement).value,
+              true
+            )
+          }
+          value={getStyle('input.padding')}
+        />
       </Fieldset>
 
       <Fieldset label="Border Radius" inputId="input-border-radius">
-        <UnitInput id="input-border-radius" />
+        <UnitInput
+          id="input-border-radius"
+          onChange={(e) =>
+            setStyle(
+              'input.border-radius',
+              (e.target as HTMLInputElement).value,
+              true
+            )
+          }
+          value={getStyle('input.border-radius')}
+        />
       </Fieldset>
 
       <Fieldset label="Color">
-        <ColorPicker />
+        <ColorPicker
+          onChange={(value) => {
+            setStyle('input.color', value)
+          }}
+          value={getStyle('input.color')}
+        />
       </Fieldset>
 
       <Fieldset label="Background">
-        <ColorPicker />
+        <ColorPicker
+          onChange={(value) => {
+            setStyle('input.background', value)
+          }}
+          value={getStyle('input.background')}
+        />
       </Fieldset>
 
       <Fieldset label="Border Color">
-        <ColorPicker />
+        <ColorPicker
+          onChange={(value) => {
+            setStyle('input.border', value)
+          }}
+          value={getStyle('input.border')}
+        />
       </Fieldset>
     </Card>
   )
